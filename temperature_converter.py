f60a1d7bd3e68226e478b4a7d08fa420a40f5b4c
kyer2680 <144353210+kyer2680@users.noreply.github.com>
2023-11-29 00:03:10 +0000

    added kelvin

--- temperature_converter.py
+++ temperature_converter.py
@@ -14,6 +14,16 @@ def f_to_c(temp_f):
     converted_f = (temp_f - 32) * (5/9)
     return converted_f
 
+# Function that converts K to F
+def k_to_f(temp_k):
+    converted_k = (temp_k - 273.15) * (9/5) + 32
+    return converted_k
+
+# Function that converts F to K
+def f_to_k(temp_f):
+    converted_f = (temp_f - 459.67) * (5/9)
+    return converted_f
+
 # Main Function that uses Conditionals to decide which convert function to select
 def main():
     if(unit == "C"):
@@ -22,10 +32,14 @@ def main():
     elif(unit == "F"):
         fahrenheit_to_celsius = f_to_c(value)
         return fahrenheit_to_celsius
+    elif(unit == "K"):
+        kelvin_to_fahrenheit = k_to_f(value)
+        return kelvin_to_fahrenheit
     else:
-        warning = "Please enter C or F to specify the unit: "
+        warning = "Please enter C, F, or K to specify the unit: "
         return warning
 
 #Print results 
 result = main()
 print("Your value is: " + str(result))
+
